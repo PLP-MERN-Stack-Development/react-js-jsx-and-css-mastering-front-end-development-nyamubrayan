55a7d1bc69fc6d70af836cf2a1e7fa8a9ab22349
brayan murangiri <137710985+nyamubrayan@users.noreply.github.com>
2025-11-04 17:23:34 +0300

    add basic pages Home,TaskManager, Posts

---
+++ vite-project/src/pages/Posts.jsx
@@ -0,0 +1,3 @@
+export default function Posts() {
+  return <h1 className="text-2xl font-bold">Posts Page</h1>;
+}
---
+++ vite-project/src/pages/TaskManager.jsx
@@ -0,0 +1,3 @@
+export default function TaskManager() {
+  return <h1 className="text-2xl font-bold">Task Manager</h1>;
+}
